9c2ca66991db7ba540dded602bc7544a5da4bdd0
shatadalsamui <shatadalsamuimain@gmail.com>
2025-08-12 23:59:39 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import { useState } from 'react';
-import ReactFlow, { ReactFlowProvider, Background, Controls } from 'reactflow';
+import ReactFlow, { ReactFlowProvider, Background, Controls, useReactFlow } from 'reactflow';
 import type { Node, Edge } from 'reactflow';
 import 'reactflow/dist/style.css';
 import { Header } from './components/Headers';
@@ -17,6 +17,50 @@ const initialNodes: Node[] = [
   },
 ];
 
+
+function FlowCanvas({ nodes, setNodes, edges }) {
+  const reactFlowInstance = useReactFlow();
+
+  // Handle drop event to add a new node at the correct canvas position
+  const onDrop = (event: React.DragEvent) => {
+    event.preventDefault();
+    const nodeType = event.dataTransfer.getData('application/reactflow');
+    if (!nodeType) return;
+
+    const reactFlowBounds = (event.target as HTMLDivElement).getBoundingClientRect();
+    const position = reactFlowInstance.project({
+      x: event.clientX - reactFlowBounds.left,
+      y: event.clientY - reactFlowBounds.top,
+    });
+    const newNode: Node = {
+      id: (nodes.length + 1).toString(),
+      type: 'default',
+      position,
+      data: { label: 'New Message' },
+    };
+    setNodes((nds) => nds.concat(newNode));
+  };
+
+  // Allow drop
+  const onDragOver = (event: React.DragEvent) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  };
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      fitView
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+    >
+      <Background />
+      <Controls />
+    </ReactFlow>
+  );
+}
+
 function App() {
   const [nodes, setNodes] = useState<Node[]>(initialNodes);
   const [edges, setEdges] = useState<Edge[]>([]);
@@ -27,10 +71,7 @@ function App() {
       <ReactFlowProvider>
         <div className="flex flex-grow">
           <main className="flex-grow h-full">
-            <ReactFlow nodes={nodes} edges={edges} fitView>
-              <Background />
-              <Controls />
-            </ReactFlow>
+            <FlowCanvas nodes={nodes} setNodes={setNodes} edges={edges} />
           </main>
           <Sidebar />
         </div>
